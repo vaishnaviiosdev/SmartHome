4298259cfa2e58a1ff344a7f6167a550b3550798
vaishnaviiosdev <vaishnavijothikumar@gmail.com>
2025-01-03 14:49:39 +0530

    Increment/Decrement Worked

--- Home/Modules/View/Cells/AirConditionerTableViewCell.swift
+++ Home/Modules/View/Cells/AirConditionerTableViewCell.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class AirConditionerTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    var temperature: Int = 18 {
+        didSet {
+            temperatureLabel.text = "\(temperature)°"
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        temperatureLabel.text = "\(temperature)°"
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+    
+    @IBAction func didIncrementButtonTapped(_ sender: Any) {
+        temperature += 1
+    }
+    
+    @IBAction func didDecrementButtonTapped(_ sender: Any) {
+        if temperature == 0 {
+            temperature = 0
+        }
+        else {
+            temperature -= 1
+        }
     }
 }
